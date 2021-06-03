33c6b389a26f6de605e44ae2c8bb0109e5596b00
Stas2402 <stasusenko24@gmail.com>
2021-06-03 22:07:19 +0300

    new structure

---
+++ Tetris/Tetris/main.py
@@ -0,0 +1,4 @@
+import tennis
+
+if __name__ == '__main__':
+    tennis.tennis()
---
+++ Tetris/Tetris/tennis.py
@@ -0,0 +1,24 @@
+def tennis():
+    import tkinter
+    import time
+    from objects import Ball, Platform, Score
+
+    tk = tkinter.Tk()
+    tk.wm_title('Tennis')
+    canvas = tkinter.Canvas(tk, width=500, height=500)
+    canvas.pack()
+
+    #x-->(width)              y^(height)
+    score = Score(canvas)
+    platform = Platform(canvas, color='green')
+    ball = Ball(canvas, platform, score, color='red')
+
+    tk.update()
+
+    while ball.not_loose:
+        ball.move()
+        platform.move()
+        tk.update()
+        time.sleep(0.01)
+
+    time.sleep(2)
